d2927c94271a2129b56932d7da3b7df09d37cef1
zhizhuoxin <shixins@umich.edu>
2023-10-10 05:57:02 -0400

    Fix the plot script.

--- scripts/performance_plot.py
+++ scripts/performance_plot.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 from pathlib import Path
 from utils import *
 
@@ -14,21 +15,32 @@ def parse_one_file(
     df = pd.read_csv(result_path)
     df["Setup"] = get_col_name(*protect_mode)
     df = df.set_index(["Setup", "name"])
-    print(df)
+    return df
 
 
 def parse_all_files(mode_list: list, bench_list: list):
     df_list = []
-    for protect_mode in all_modes:
+    for protect_mode in mode_list:
         for bench_id in bench_list:
             df_list.append(parse_one_file(protect_mode, bench_id))
 
     df = pd.concat(df_list)
     print(df)
+    return df
 
 
 def main():
-    parse_one_file([False, False], 0)
+    mode_list = [
+        [False, False],
+        [True, True]
+    ]
+    bench_list = list(range(24))
+    df = parse_all_files(mode_list, bench_list)
+
+    ax = sns.barplot(x="name", y="mean (ns)", hue="Setup", data=df)
+    plot_dir = proj_dir / "scripts" / "plot"
+    plot_dir.mkdir(exist_ok=True)
+    plt.savefig(plot_dir / "performance.pdf")
 
 
 if __name__ == '__main__':
