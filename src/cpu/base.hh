1926c826949fb69f730f0391eac7fbf3e36d278b
zhizhuoxin <shixins@umich.edu>
2024-03-05 11:14:07 -0500

    Fix config for protecting KASLR.

--- src/cpu/base.hh
+++ src/cpu/base.hh
@@ -126,9 +126,9 @@ public:
     };
 
     bool protectKaslr[NumKaslrRegionType];
-    Addr regionStart[NumKaslrRegionType] = {0xffffff8000000000, 0xffffffffc0000000};
-    Addr regionSize[NumKaslrRegionType] = {0x6f00000000, 0x3f000000};
-    Addr regionAlignBits[NumKaslrRegionType] = {30, 25};
+    Addr regionStart[NumKaslrRegionType] = {0xffffff8000000000, 0xfffffe8000000000};
+    Addr regionSize[NumKaslrRegionType] = {0x6000000000, 0x8000000000};
+    Addr regionAlignBits[NumKaslrRegionType] = {30, 30};
     Addr regionMask[NumKaslrRegionType] = {0, 0};
 
     // TODO: In the final design, we should not set kaslrOffset here.
