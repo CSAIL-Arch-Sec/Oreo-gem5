dd2d119fb0c7549d3f31494eabfb75f782efd318
shixins <shixins@mit.edu>
2024-09-22 01:01:07 -0400

    Update parse_spec.py.

--- scripts/parse_spec.py
+++ scripts/parse_spec.py
@@ -14,11 +14,13 @@ useful_columns = {
     "board.processor.cores0.core.quiesceCycles": "0.quiesceCycles",
     "board.processor.cores0.core.committedInsts": "0.committedInsts",
     "board.processor.cores0.core.cpi": "0.cpi",
+    "board.processor.cores0.core.ipc": "0.ipc",
     "board.processor.cores1.core.numCycles": "1.numCycles",
     "board.processor.cores1.core.idleCycles": "1.idleCycles",
     "board.processor.cores1.core.quiesceCycles": "1.quiesceCycles",
     "board.processor.cores1.core.committedInsts": "1.committedInsts",
-    "board.processor.cores1.core.cpi": "1.cpi ",
+    "board.processor.cores1.core.cpi": "1.cpi",
+    "board.processor.cores1.core.ipc": "1.ipc",
 }
 
 default_setup_map = {
@@ -60,16 +62,16 @@ def parse_stats_lines(lines: list, name: str):
         x = parse_stats_line(line)
         if x is not None:
             data.append(x)
-    print("Skip #lines", len(lines) - len(data))
+    # print("Skip #lines", len(lines) - len(data))
     df = pd.DataFrame(data=data).set_index(0).transpose()
     df.rename(columns=useful_columns, inplace=True)
     df = df[useful_columns.values()]
     df["name"] = name
-    print(df)
+    # print(df)
     return df
 
 
-def parse_all(input_dir: Path, roi_idx: int, setup_map: dict, benchmark_list: list, benchmark_suffix: str):
+def parse_all(input_dir: Path, roi_idx: int, expected_stats: int, setup_map: dict, benchmark_list: list, benchmark_suffix: str):
     setup_df_list = []
     for setup_name, setup_dir_name in setup_map.items():
         df_list = []
@@ -77,8 +79,12 @@ def parse_all(input_dir: Path, roi_idx: int, setup_map: dict, benchmark_list: li
             stats_path = input_dir / setup_dir_name / f"{benchmark}{benchmark_suffix}" / "stats.txt"
             with stats_path.open() as stats_file:
                 lines = stats_file.readlines()
-            lines = split_stats(lines)[roi_idx]
-            df_list.append(parse_stats_lines(lines, benchmark))
+            split_lines = split_stats(lines)
+            if len(split_lines) == expected_stats:
+                lines = split_lines[roi_idx]
+                df_list.append(parse_stats_lines(lines, benchmark))
+            else:
+                print(f"Do not have enough roi for config {setup_name} benchmark {benchmark}")
         df = pd.concat(df_list)
         df["setup"] = setup_name
         setup_df_list.append(df)
@@ -86,23 +92,75 @@ def parse_all(input_dir: Path, roi_idx: int, setup_map: dict, benchmark_list: li
     return df
 
 
-def main():
+def cal_overhead(df: pd.DataFrame, term_name: str, need_avg: bool):
+    if need_avg:
+        df[term_name] = (df[f"0.{term_name}"] + df[f"1.{term_name}"]) / 2
+
+    baseline_df = df.loc[df["setup"] == "Baseline"]
+    oreo_df = df.loc[df["setup"] == "Oreo"]
+
+    baseline_df.set_index("name", inplace=True)
+    oreo_df.set_index("name", inplace=True)
+    # print(baseline_df)
+    # print(oreo_df)
+
+    cmp_df = pd.DataFrame()
+    cmp_df["Baseline"] = baseline_df[term_name]
+    cmp_df["Oreo"] = oreo_df[term_name]
+    cmp_df = cmp_df[cmp_df[["Baseline", "Oreo"]].notnull().all(1)]
+    cmp_df["Overhead"] = (cmp_df["Oreo"] - cmp_df["Baseline"]) / cmp_df["Baseline"]
+    print(cmp_df)
+    print(cmp_df.mean())
+
+
+@click.command()
+@click.option(
+    "--parse-raw",
+    is_flag=True,
+)
+def main(parse_raw: bool):
     raw_result_dir = proj_dir / "result"
     output_dir = script_dir / "spec_output"
     output_dir.mkdir(exist_ok=True)
 
     # benchmark_list = ["401.bzip2"]
-    benchmark_list = spec_bench_list
+    benchmark_list = spec2006_bench_list
     benchmark_suffix = "_0"
 
-    df = parse_all(
-        input_dir=raw_result_dir,
-        roi_idx=1,
-        setup_map=default_setup_map,
-        benchmark_list=benchmark_list,
-        benchmark_suffix=benchmark_suffix
-    )
-    df.to_csv(output_dir / "test.csv")
+    # import subprocess
+    # server_path = Path("/home/shixins/protect-kaslr/gem5-new/result")
+    # for setup_dir_name in default_setup_map.values():
+    #     result_path = raw_result_dir / setup_dir_name
+    #     for benchmark in spec2006_bench_list:
+    #         cmd = f"scp -r shixins@dobby.csail.mit.edu:{server_path / setup_dir_name / f"{benchmark}{benchmark_suffix}"} {result_path}"
+    #         print(cmd)
+    #         subprocess.run(cmd, shell=True)
+
+    # for setup_dir_name in default_setup_map.values():
+    #     for benchmark in spec2006_bench_list:
+    #         output_path = raw_result_dir / setup_dir_name / f"{benchmark}{benchmark_suffix}" / "board.pc.com_1.device"
+    #         with output_path.open() as output_file:
+    #             s = output_file.read()
+    #         if re.search("Error loading parse.pl!", s) is not None:
+    #             print("Fail", setup_dir_name, benchmark)
+    #         else:
+    #             print("Success", setup_dir_name, benchmark)
+
+    if parse_raw:
+        df = parse_all(
+            input_dir=raw_result_dir,
+            roi_idx=1,
+            expected_stats=3,
+            setup_map=default_setup_map,
+            benchmark_list=benchmark_list,
+            benchmark_suffix=benchmark_suffix
+        )
+        df.to_csv(output_dir / "test.csv")
+    else:
+        df = pd.read_csv(output_dir / "test.csv")
+
+        cal_overhead(df, "cpi", True)
+        cal_overhead(df, "ipc", True)
 
 
 if __name__ == '__main__':
